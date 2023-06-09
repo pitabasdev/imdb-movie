1bb1bb3611f68ad3dfdeb736e33f9b5c68ae909f
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2023-06-09 18:24:52 +0530

    install package

--- src/components/Home page/Home/Home.js
+++ src/components/Home page/Home/Home.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React ,{useEffect} from "react";
+import { useState } from "react";
 function Home(){
+    const [popularMovie, setMovie]=useState([]);
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
+        .then(res => res.json())
+        .then(data => console.log(data.results))
+    }, [])
     return(
         <div>
             <h1>Home</h1>
