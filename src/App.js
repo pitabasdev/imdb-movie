1217c61d2a0d1665f1258b4ce3c0bcf1bfa890ac
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2023-06-09 18:15:49 +0530

    Create Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router,Route,Routes } from "react-router-dom"
 import "./App.css"
 import Header from "./components/Header/Header"
+import Home from "./components/Home page/Home/Home"
 function App(){
   return(
     <div>
       <Router>
         <Header/>
         <Routes>
-          <Route path="/" element={<h1>Hello</h1>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/movie/:id" element={<h1>About</h1>}/>
           <Route path="/movie/:type" element={<h1>Contact</h1>}/>
           <Route path="/*" element={<h1>Error</h1>} ></Route>
